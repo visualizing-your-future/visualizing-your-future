6fc539c785cbbfaf79a8d76c03180ffe618b4e53
bri111 <brianall@hawaii.edu>
2024-09-09 22:12:07 -1000

    navbar update

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -15,9 +15,15 @@ const NavBar = () => {
   }), []);
   const menuStyle = { marginBottom: '0px' };
   return (
-    <Navbar bg="light" expand="lg" style={menuStyle}>
-      <Container>
-        <Navbar.Brand id={COMPONENT_IDS.NAVBAR_LANDING_PAGE} as={NavLink} to="/"><h1>meteor-application-template-production</h1></Navbar.Brand>
+    <Navbar bg="light" expand="lg" style={menuStyle} className="justify-content-center px-5">
+      <Container className="d-flex justify-content-between custom-navbar-spacing">
+        <Navbar.Brand id={COMPONENT_IDS.NAVBAR_LANDING_PAGE} as={NavLink} to="/">
+          <img src="/images/logo.png" className="rounded" alt="visualizing your future logo" width={90} />
+          <div className="logo-text ms-2">
+            <span className="fw-bold">VISUALIZING</span>
+            <span className="fw-bold">YOUR FUTURE</span>
+          </div>
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls={COMPONENT_IDS.NAVBAR_COLLAPSE} />
         <Navbar.Collapse id={COMPONENT_IDS.NAVBAR_COLLAPSE}>
           <Nav className="me-auto justify-content-start">
@@ -37,7 +43,7 @@ const NavBar = () => {
           </Nav>
           <Nav className="justify-content-end">
             {currentUser === '' ? (
-              <NavDropdown id={COMPONENT_IDS.NAVBAR_LOGIN_DROPDOWN} title="Login">
+              <NavDropdown id={COMPONENT_IDS.NAVBAR_LOGIN_DROPDOWN} title="Login" className="custom-login-button">
                 <NavDropdown.Item id={COMPONENT_IDS.NAVBAR_LOGIN_DROPDOWN_SIGN_IN} as={NavLink} to="/signin"><PersonFill />Sign in</NavDropdown.Item>
                 <NavDropdown.Item id={COMPONENT_IDS.NAVBAR_LOGIN_DROPDOWN_SIGN_UP} as={NavLink} to="/signup"><PersonPlusFill />Sign up</NavDropdown.Item>
               </NavDropdown>
