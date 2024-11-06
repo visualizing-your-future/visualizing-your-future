d24ae809cbfc2ff238c6ce281ea8cca2658e702f
Victoria Valverde <156735868+vavalverde@users.noreply.github.com>
2024-11-06 08:35:47 -1000

    Added Projections

--- app/imports/ui/pages/VisualizationExport.jsx
+++ app/imports/ui/pages/VisualizationExport.jsx
@@ -18,7 +18,7 @@ const snapshotData = [{
   debt: 66193143,
   opex: 35603263,
   liquidity: 422605819,
-  perpetuity: 0,
+  perpetuity: 1,
   cashInFlow: 35693705,
   cashOutFlow: 36110120,
   netCashFlow: 416415,
@@ -34,7 +34,7 @@ const snapshotData = [{
   debt: 41686286,
   opex: 36014771,
   liquidity: 432669418,
-  perpetuity: 0,
+  perpetuity: 1,
   cashInFlow: 35567019,
   cashOutFlow: 36521628,
   netCashFlow: 954609,
@@ -50,7 +50,7 @@ const snapshotData = [{
   debt: 41179429,
   opex: 36803493,
   liquidity: 428237517,
-  perpetuity: 0,
+  perpetuity: 1,
   cashInFlow: 35914282,
   cashOutFlow: 37310350,
   netCashFlow: 1396068,
@@ -66,7 +66,7 @@ const snapshotData = [{
   debt: 37672571,
   opex: 37218414,
   liquidity: 440034270,
-  perpetuity: 0,
+  perpetuity: 1,
   cashInFlow: 36670341,
   cashOutFlow: 37725271,
   netCashFlow: 1054930,
@@ -82,7 +82,7 @@ const snapshotData = [{
   debt: 34165714,
   opex: 37666058,
   liquidity: 444906527,
-  perpetuity: 0,
+  perpetuity: 1,
   cashInFlow: 37234517,
   cashOutFlow: 38172915,
   netCashFlow: 938399,
@@ -98,7 +98,7 @@ const snapshotData = [{
   debt: 30658857,
   opex: 38215618,
   liquidity: 456901296,
-  perpetuity: 0,
+  perpetuity: 1,
   cashInFlow: 38102065,
   cashOutFlow: 38722475,
   netCashFlow: 620410,
@@ -114,7 +114,7 @@ const snapshotData = [{
   debt: 27152000,
   opex: 38832666,
   liquidity: 461740759,
-  perpetuity: 0,
+  perpetuity: 1,
   cashInFlow: 38997851,
   cashOutFlow: 39339523,
   netCashFlow: 341672,
@@ -130,7 +130,7 @@ const snapshotData = [{
   debt: 24000000,
   opex: 39532314,
   liquidity: 473557604,
-  perpetuity: 0,
+  perpetuity: 1,
   cashInFlow: 39920149,
   cashOutFlow: 39684314,
   netCashFlow: 235835,
@@ -146,7 +146,7 @@ const snapshotData = [{
   debt: 21000000,
   opex: 40180479,
   liquidity: 477532588,
-  perpetuity: 0,
+  perpetuity: 1,
   cashInFlow: 40349674,
   cashOutFlow: 40180479,
   netCashFlow: 169195,
@@ -162,7 +162,7 @@ const snapshotData = [{
   debt: 18000000,
   opex: 40840605,
   liquidity: 488714889,
-  perpetuity: 0,
+  perpetuity: 1,
   cashInFlow: 40751477,
   cashOutFlow: 40840605,
   netCashFlow: 89128,
@@ -178,7 +178,7 @@ const snapshotData = [{
   debt: 15000000,
   opex: 41534037,
   liquidity: 492534740,
-  perpetuity: 0,
+  perpetuity: 1,
   cashInFlow: 41142891,
   cashOutFlow: 41534037,
   netCashFlow: 391145,
@@ -194,7 +194,7 @@ const snapshotData = [{
   debt: 12000000,
   opex: 42249646,
   liquidity: 503602972,
-  perpetuity: 0,
+  perpetuity: 1,
   cashInFlow: 41534656,
   cashOutFlow: 42249646,
   netCashFlow: 714990,
@@ -211,13 +211,13 @@ const dataSets = {
 };
 
 // Comparison Component
-// eslint-disable-next-line react/prop-types
 const Comparison = ({ data }) => {
   const [selectedYears, setSelectedYears] = useState([]);
   const [selectedMetric1, setSelectedMetric1] = useState('netPosition');
   const [selectedMetric2, setSelectedMetric2] = useState('assets');
-  // eslint-disable-next-line no-unused-vars
-  const [selectedMetric3, setSelectedMetric3] = useState('debt');
+  const [projectionPercentage, setProjectionPercentage] = useState(0);
+  const [projectionYears, setProjectionYears] = useState(0);
+  const [showProjection, setShowProjection] = useState(false);
 
   const handleYearSelect = (year) => {
     setSelectedYears((prev) => {
@@ -234,16 +234,38 @@ const Comparison = ({ data }) => {
     }
   };
 
-  // eslint-disable-next-line react/prop-types
+  const handleProjectionToggle = () => setShowProjection(!showProjection);
+
+  const calculateProjections = (baseYearData) => {
+    const projections = [];
+    let lastValue1 = baseYearData[selectedMetric1];
+    let lastValue2 = baseYearData[selectedMetric2];
+
+    for (let i = 1; i <= projectionYears; i++) {
+      lastValue1 *= 1 + projectionPercentage / 100;
+      lastValue2 *= 1 + projectionPercentage / 100;
+
+      projections.push({
+        year: (parseInt(baseYearData.year) + i).toString(),
+        [selectedMetric1]: lastValue1,
+        [selectedMetric2]: lastValue2,
+      });
+    }
+
+    return projections;
+  };
+
+
   const filteredData = data.filter((entry) => selectedYears.includes(entry.year));
+  const baseYearData = filteredData[0]; // Use the first selected year as base for projections
+
+  const projectionData = showProjection && baseYearData ? calculateProjections(baseYearData) : [];
 
   return (
     <Card.Body>
       <h5>Select Years to Compare</h5>
       <Form>
-        {/* eslint-disable-next-line react/prop-types */}
         <div style={{ display: 'flex', flexWrap: 'nowrap', overflowX: 'auto' }}>
-          {/* eslint-disable-next-line react/prop-types */}
           {data.map((entry, index) => (
             <Button
               key={index}
@@ -260,13 +282,13 @@ const Comparison = ({ data }) => {
           <Form.Label>Choose First Metric:</Form.Label>
           <Form.Control as="select" value={selectedMetric1} onChange={(e) => handleMetricChange(e.target.value, true)}>
             <option value="assets">Assets</option>
-            <option value="liabilitites">Liabilities</option>
+            <option value="liabilities">Liabilities</option>
             <option value="netPosition">Net Position</option>
             <option value="cashOnHand">Cash on Hand</option>
             <option value="debt">Debt</option>
             <option value="opex">Opex</option>
             <option value="liquidity">Liquidity</option>
-            <option value="perpetuitiy">Perpetuity</option>
+            <option value="perpetuity">Perpetuity</option>
             <option value="cashInFlow">Cash In Flow</option>
             <option value="cashOutFlow">Cash Out Flow</option>
             <option value="netCashFlow">Net Cash Flow</option>
@@ -294,6 +316,44 @@ const Comparison = ({ data }) => {
             <option value="changeAE">Change of Actual + Encumbrance</option>
           </Form.Control>
         </Form.Group>
+        <Form.Group controlId="projectionToggle">
+          <Form.Check
+            type="switch"
+            label="Show Projections"
+            checked={showProjection}
+            onChange={handleProjectionToggle}
+          />
+        </Form.Group>
+        {showProjection && (
+          <Form.Group controlId="projectionControls">
+            <Form.Label>Projection Percentage Increase:</Form.Label>
+            <Form.Control
+              type="number"
+              value={projectionPercentage}
+              onChange={(e) => setProjectionPercentage(parseFloat(e.target.value))}
+            />
+            <Form.Label>Number of Years for Projection:</Form.Label>
+            <Form.Control
+              type="number"
+              value={projectionYears}
+              onChange={(e) => setProjectionYears(parseInt(e.target.value, 10))}
+            />
+          </Form.Group>
+        )}
+        {filteredData.length > 0 && (
+          <div style={{ marginTop: '40px', height: '110%' }}>
+            <ChartComponent
+              data={[...filteredData, ...projectionData]}
+              chartType="dual"
+              key1={selectedMetric1}
+              key2={selectedMetric2}
+              color1="#8884d8"
+              color2="#82ca9d"
+              width={400}
+              height={400}
+            />
+          </div>
+        )}
       </Form>
       {filteredData.length > 0 && (
         <div style={{ marginTop: '40px', height: '110%' }}> {/* Keep square aspect ratio */}
